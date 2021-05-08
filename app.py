54272fb832de374010c2a683b00ec6e60e012205
Joseph Velazco Rodríguez <viajero35173@innovaccion.mx>
2021-05-08 14:46:48 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+from unicodedata import normalize
+from os import environ
+import re
+
+app=Flask(__name__)
+
+bot=ChatBot('JP', read_only=True,
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    input_adapter="chatterbot.input.VariableInputTypeAdapter",
+    output_adapter="chatterbot.output.OutputAdapter",
+    output_format="text",
+    database_uri='sqlite:///JP_db.db',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response':'Lo siento, eso está fuera de mi enfoque. Pero puedes preguntarme algo sobre el coronavirus.',
+            'maximum_similarity_treshold':0.999999,
+        },
+    ]
+)
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText=request.args.get('msg')
+    userText=re.sub(r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", normalize( "NFD", userText), 0, re.I)
+    userText= normalize( 'NFC', userText)
+    userText=userText.lower()
+    return str(bot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
